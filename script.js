dd47d80eeec2d5c77ea928886362b90fd67459c0
George J <george@gjnsn.com>
2025-03-13 21:44:34 -0600

    kinda tested progress

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
-function get_cur_date() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0'); // Month is 0-indexed
-    const day = String(today.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
+const region_aspect_mapping = {
+        "n": [338,23],
+        "ne": [23,68],
+        "e": [68,113],
+        "se": [113,158],
+        "s": [158,203],
+        "sw": [203,248],
+        "w": [248,293],
+        "nw": [293,338]
+    }
 
 function makeNonNegative(inputElement) {
     inputElement.addEventListener('input', () => {
@@ -38,13 +41,161 @@ async function getJsonFromEndpoint(data) {
     }
 }
 
+function create_geojson(rules) {
+    // rules = rules + get_helper_layers()
+    // TODO: ADD HELPER LAYERS TO rules
+
+    return {
+        "features": rules.map((rule) => {
+            return {
+                "geometry": null,
+                "id": crypto.randomUUID(),
+                "type": "Feature",
+                "properties": {
+                    "alias": rule.rule,
+                    "title": rule.title,
+                    "class": "ConfiguredLayer"
+                }
+            }}),
+        "type": "FeatureCollection"
+    }
+}
+
+function save_as_json(exportObj, exportName){
+    // https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
+
+function get_specific_avaforecast(data, areaId) {
+    for(const obj of data) {
+        if(obj.areaId == areaId) {
+            return obj
+        }
+    }
+    console.log("fucking kill myself");
+}
+
+function get_treecover_bounds() {
+    // fill into { "[elev]": [min,max],...}
+    return {
+        "alp": [treecoverAlpMin, treecoverAlpMax],
+        "tln": [treecoverTlnMin, treecoverTlnMax],
+        "btl": [treecoverBtlMin, treecoverBtlMax],
+    }
+}
+
+function sort_trim_aspects(aspects) {
+    /**
+     * Returns the start and end aspect
+     * Returns null if aspects is empty
+     */
+    if (aspects.length === 0) {
+        return null;
+    }
+    
+    let aspectRegions = aspects.map(asp => region_aspect_mapping[asp]);
+    let ret = [];
+    let i = 0;
+    
+    while (aspectRegions.length > 0) {
+        if (i > aspectRegions.length - 1) i = 0;
+
+        if (ret.length === 0) {
+            ret.push(aspectRegions[i]);
+            aspectRegions.splice(i, 1);
+        }
+
+        let [boundL, boundR] = aspectRegions[i] || [];
+        for (let j = 0; j < ret.length; j++) {
+            let [regL, regR] = ret[j];
+            if (boundL === regR) {
+                ret.splice(j + 1, 0, aspectRegions[i]);
+                aspectRegions.splice(i, 1);
+                break;
+            } else if (boundR === regL) {
+                ret.splice(j, 0, aspectRegions[i]);
+                aspectRegions.splice(i, 1);
+                break;
+            }
+        }
+        i++;
+    }
+    return [ret[0][0], ret[ret.length - 1][1]];
+}
+
+
+function rule_tool(mode, range) {
+    return mode + range.join("-");
+}
+
+function format_as_rule(aspect_range, treecover_range, color) {
+    return  rule_tool("s", [slideSlopeMin.value, slideSlopeMax.value]) +
+            rule_tool("a", aspect_range) +
+            rule_tool("t", treecover_range) +
+            "c" + color + "p";
+}
+
+function split_by_elevation(aspectElevations) {
+    let ret = {
+        "alp": [],
+        "tln": [],
+        "btl": [],
+    };
+    for(const e of aspectElevations) {
+        div = e.split("_");
+        ret[div[1]].push(div[0]);
+    }
+    return ret;
+}
+
+function danger_to_color(likelihood, expectedSize) {
+    // TODO: impl
+}
+
+
+function danger_to_rule(problem, date) {
+    var aspects = split_by_elevation(problem.aspectElevations);
+    const color = "FF0000"; // danger_to_color(null); // TODO: implement color mapping from danger levels
+    const treecovers = get_treecover_bounds(); //
+    rule = {
+        "title": "",
+        "rule": "sc_"
+    };
+    for(const key of Object.keys(aspects)) {
+        // per elev, get aspect bounds
+        aspects[key] = sort_trim_aspects(aspects[key]);
+        if(aspects[key]) {
+            // only add rule if prob exists for elev
+            rule.title = " " + problem.type + " " + date;
+            // rule.title = " " + problem.type + " " + date.toISOString().split("T")[0];
+            rule.rule += format_as_rule(aspects[key], treecovers[key], color);
+            // rule.rule += "p";
+        }
+    }
+    return rule;
+}   
+
+function interpret_problems(problems, date) {
+    let result = [];
+    for(let i = 0; i < problems.length; i++) {
+        result.push(danger_to_rule(problems[i], date))
+    }
+    return result
+}
+
 async function avy_forecast(date = null, and_weather = false) {
     let params = {};
     if (!and_weather) {
         params["productType"] = "avalancheforecast";
     }
     if (date) {
-        params["datetime"] = date.toISOString(); // Convert Date to ISO string
+        params["datetime"] = date.toISOString();
     }
 
     var data = "/products/all?"
@@ -55,7 +206,7 @@ async function avy_forecast(date = null, and_weather = false) {
     try {
         const resp = await getJsonFromEndpoint(data);
         const ret = [];
-
+        
         if (resp && Array.isArray(resp)) { // Check if resp is an array
             for (const item of resp) {
                 if (
@@ -65,6 +216,7 @@ async function avy_forecast(date = null, and_weather = false) {
                 ) {
                     ret.push(item); // Just push the raw object
                 } else if (typeof item === "object" && item !== null) {
+                    // TODO: handle else
                     ret.push(item); // Just push the raw object
                 }
             }
@@ -83,7 +235,7 @@ async function avy_regions(date = null, and_weather = false) {
         params["productType"] = "avalancheforecast";
     }
     if (date) {
-        params["datetime"] = date.toISOString(); // Convert Date to ISO string
+        params["datetime"] = date.toISOString();
     }
 
     var data = "/products/all/area?"
@@ -95,15 +247,16 @@ async function avy_regions(date = null, and_weather = false) {
         const resp = await getJsonFromEndpoint(data);
         const ret = [];
 
-        if (resp && Array.isArray(resp)) { // Check if resp is an array
-            for (const item of resp) {
+        if (resp && resp.features && Array.isArray(resp.features)) { // Check if resp is an array
+            for (const item of resp.features) {
                 if (
                     typeof item === "object" &&
                     item !== null &&
-                    item.type === "avalancheforecast"
+                    item.type === "Feature"
                 ) {
                     ret.push(item); // Just push the raw object
                 } else if (typeof item === "object" && item !== null) {
+                    // TODO: handle else
                     ret.push(item); // Just push the raw object
                 }
             }
@@ -127,8 +280,8 @@ function find_region_for_point(point, regions) {
         return null;
     }
 
-    const turfPoint = turf.point(...point);
-
+    const turfPoint = turf.point(point);
+    let ret = "";
     for (const region of regions) {
         if (typeof region !== 'object' || region === null || !region.geometry || !region.geometry.type || region.geometry.type !== 'MultiPolygon') {
             console.warn("Invalid GeoJSON polygon encountered in regions array:", region);
@@ -137,93 +290,136 @@ function find_region_for_point(point, regions) {
 
         try {
             const mpolygon = turf.multiPolygon(region.geometry.coordinates); // Create Turf polygon
-            const isInside = booleanPointInPolygon(turfPoint, mpolygon);
+            const isInside = turf.booleanPointInPolygon(turfPoint, mpolygon);
 
             if (isInside) {
-                return region.id; // Or region.properties.id, if ID is in properties
+                console.log(region.id,region);
+                ret = region.id;
+                // return region.id; // Or region.properties.id, if ID is in properties
             }
         } catch (error) {
             console.error("Error checking point in polygon:", error);
         }
     }
-
-    return null; // No region found
+    return ret;
+    // return null; // No region found
 }
 
-function main(lat,lon,date) {
-    const forecast = avy_forecast();
-    const regions = avy_regions();
+async function main(lat,lon,date) {
+    let date_param = null;
+    if(date) {
+        // if date given, use 12:00:00
+        // this will guarantee for any date we  
+        // TODO: TEST
+        // always return the forcast FOR that day, not FROM that day
+        const split = date.split("-");
+        date_param = new Date();
+        console.log(date, date_param)
+        date_param.setYear(split[0]);
+        date_param.setUTCMonth(split[1]-1);
+        date_param.setUTCDate(split[2]);
+        date_param.setUTCHours(12,0,0,0);
+        console.log(date_param)
+
+    } else {
+        // if no date given, $OMIT$ use current day AND TIME
+        date_param = new Date();
+        date = date_param.toISOString().split("T")[0];
+        console.log(date)
+        // 10:30PM UTC
+        // date.setHours(23);
+        // console.log(date.toISOString());
+    }
+
+    const forecast = await avy_forecast(date_param);
+    const regions = await avy_regions(date_param);
+    console.log("forecast", forecast);
+    console.log("regions", regions);
+
+    treecoverAlpMin = 0;
+    treecoverAlpMax = treecoverAlpTln.value;
+    treecoverTlnMin = treecoverAlpMax;
+    treecoverTlnMax = treecoverTlnBtl.value;
+    treecoverBtlMin = treecoverTlnMax;
+    treecoverBtlMax = 100;
 
     const zone_id = find_region_for_point([lon,lat],regions);
     console.log(zone_id);
+    const zone_forecast = get_specific_avaforecast(forecast, zone_id);
+    
+    const day_probs = zone_forecast.avalancheProblems.days[0];
+    const interpretation = interpret_problems(day_probs, date)
+    console.log("main",zone_id,zone_forecast,interpretation);
+    const json_data = create_geojson(interpretation);
+    save_as_json(json_data, "ava_shading_" + date);
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const forecastDateInput = document.getElementById('forecast-date');
-    // forecastDateInput.value = get_cur_date();
-    
-    const includeHelpersContainer = document.getElementById('include-helpers-container');
     const getForecastButton = document.getElementById('get-forecast-button');
     
-    
-    const treecoverAlpMin = document.getElementById('treecover-alp-min');
-    const treecoverAlpMax = document.getElementById('treecover-alp-max');
-    const treecoverTlnMin = document.getElementById('treecover-tln-min');
-    const treecoverTlnMax = document.getElementById('treecover-tln-max');
-    const treecoverBtlMin = document.getElementById('treecover-btl-min');
-    const treecoverBtlMax = document.getElementById('treecover-btl-max');
-    const slideSlopeMin = document.getElementById('slide-slope-min');
-    const slideSlopeMax = document.getElementById('slide-slope-max');
+    // dropdown nonsense
+    const coll = document.getElementsByClassName("collapsible");
 
-    makeNonNegative(treecoverAlpMin);
-    makeNonNegative(treecoverAlpMax);
-    makeNonNegative(treecoverTlnMin);
-    makeNonNegative(treecoverTlnMax);
-    makeNonNegative(treecoverBtlMin);
-    makeNonNegative(treecoverBtlMax);
+    for(let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.display === "flex") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "flex";
+            }
+        });
+    }
+
+    treecoverAlpTln = document.getElementById('treecover-alp-tln');
+    treecoverTlnBtl = document.getElementById('treecover-tln-btl');
+    slideSlopeMin = document.getElementById('slide-slope-min');
+    slideSlopeMax = document.getElementById('slide-slope-max');
+
+    makeNonNegative(treecoverAlpTln);
+    makeNonNegative(treecoverTlnBtl);
     makeNonNegative(slideSlopeMin);
     makeNonNegative(slideSlopeMax);
 
-    treecoverAlpMax.addEventListener('input', () => {
-        treecoverTlnMin.value = treecoverAlpMax.value;
+    treecoverAlpTln.addEventListener('input', () => {
+        makeNonNegative(treecoverAlpTln);
+        if(treecoverAlpTln.value > treecoverTlnBtl.value) {
+            treecoverAlpTln.value = treecoverTlnBtl.value;
+        }
     });
-
-    treecoverTlnMin.addEventListener('input', () => {
-        treecoverAlpMax.value = treecoverTlnMin.value;
+    
+    treecoverTlnBtl.addEventListener('input', () => {
+        makeNonNegative(treecoverTlnBtl);
+        if(treecoverTlnBtl.value < treecoverAlpTln.value) {
+            treecoverTlnBtl.value = treecoverAlpTln.value;
+        }
     });
-
-    treecoverTlnMax.addEventListener('input', () => {
-        treecoverBtlMin.value = treecoverTlnMax.value;
-    });
-
-    treecoverBtlMin.addEventListener('input', () => {
-        treecoverTlnMax.value = treecoverBtlMin.value;
-    });
-
-    getForecastButton.addEventListener('click', () => {
-        const lat = document.getElementById('latitude').value;
-        const lon = document.getElementById('longitude').value;
-        const date = document.getElementById('forecast-date').value;
+    
+    getForecastButton.addEventListener('click', async () => {
+        let lat = document.getElementById('latitude').value;
+        let lon = document.getElementById('longitude').value;
+        let date = forecastDateInput.value;
         if (lat === "" || lon === "") {
             // handle no lat/lon
             return
         }
 
-        main(lat,lon,date)
+        await main(lat,lon,date)
     })
 });
 
-const coll = document.getElementsByClassName("collapsible");
-        let i;
-
-        for (i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                const content = this.nextElementSibling;
-                if (content.style.display === "block") {
-                    content.style.display = "none";
-                } else {
-                    content.style.display = "block";
-                }
-            });
-        }
+function reset_settings() {
+    const settings = {
+        treecoverAlpTln: 1,
+        treecoverTlnBtl: 25,
+        slideSlopeMin: 30,
+        slideSlopeMax: 50,
+    }
+    treecoverAlpTln.value = settings.treecoverAlpTln;
+    treecoverTlnBtl.value = settings.treecoverTlnBtl;
+    slideSlopeMin.value = settings.slideSlopeMin;
+    slideSlopeMax.value = settings.slideSlopeMax;
+}
